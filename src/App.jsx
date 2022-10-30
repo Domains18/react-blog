f761b382e9a9b37ec87f359d70fdf87c3a943874
Domains18 <gibsonsgibson88@gmail.com>
2022-10-30 12:54:02 +0300

    adding file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Nav/>
+    </>
   );
 }
 
