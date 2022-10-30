67e604644dc5c740af1ac6d9e7046d5d33e83fc5
Domains18 <gibsonsgibson88@gmail.com>
2022-10-30 23:35:33 +0300

    nav styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './nav.css'
 const Nav = () => {
   return (
-    <section>
-        <h1> Nav</h1>
+    <section className='nav'>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contacts</a></li>
+        <li><a href="">Posts</a></li>
+      </ul>
     </section>
   )
 }
