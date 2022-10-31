3438a44cc533cc791c65e086d19603cd7d2dfe95
Domains18 <gibsonsgibson88@gmail.com>
2022-10-31 23:42:39 +0300

    nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <section className='nav'>
       <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#Contacts">Contacts</a></li>
         <li><a href="#Posts">Posts</a></li>
