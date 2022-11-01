ac43ddaeb4f9a0be756bb31652910c3cc51a7d7b
Manfred <manfredkyp@gmail.com>
2022-11-01 23:19:40 +0300

    created post page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,30 @@ import Popular from "./components/PopularPosts/Popular";
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact/Contacts";
 import About from "./components/About/About";
+import Posts from "./components/Posts/Posts";
+import Styled from "styled-components";
+
+
+
 function App() {
   return (
     <>
-      <Nav/>
-      <Header/>      
-      <Popular />
-      <About/>
-      <Contact/>
-      <Footer/>
+      <Nav />
+      <Container>
+        <Header />
+        <Popular />
+        <About />
+        <Posts />
+        <Contact />
+      </Container>
+      <Footer />
     </>
   );
 }
+
+const Container = Styled.div`
+  max-width:1200px;
+  margin:auto;
+`;
 
 export default App;
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,11 +1,81 @@
 import React from 'react'
 import './Posts.css'
+import image from '../../images/posts/good-morning.jpg';
+import image2 from "../../images/posts/r.jpg";
+import image3 from "../../images/posts/pexels.jpg";
+
+
 const Posts = () => {
   return (
-    <section>
-        <h1>Posts</h1>
+    <section className="post_section">
+      <div className="post_title">
+        <h1>Trending Posts</h1>
+      </div>
+      <div className="row">
+        <div className="post_trend">
+          {/* post item */}
+          <div className="post_item">
+            <div className="post_body">
+              <div className="post_img">
+                <img src={image2} alt="" />
+              </div>
+              <div className="post_content">
+                <h4>Post One title</h4>
+                <p>Post one this one</p>
+                <a href="https://post.one" className="btn-1">
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* end post */}
+          {/* post item */}
+          <div className="post_item">
+            <div className="post_body">
+              <div className="post_img">
+                <img
+                  src={image}
+                  alt=""
+                />
+              </div>
+              <div className="post_content">
+                <h4>Post One title</h4>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae
+                  molestiae architecto pariatur dolorum officiis obcaecati
+                  veritatis delectus repellat beatae reprehenderit?
+                </p>
+                <a href="https://post.one" className="btn-1">
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* end post */}
+          {/* post item */}
+          <div className="post_item">
+            <div className="post_body">
+              <div className="post_img">
+                <img src={image3} alt="" />
+              </div>
+              <div className="post_content">
+                <h4>Post One title</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam, quam!
+                </p>
+                <a href="https://post.one" className="btn-1">
+                  Read more
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* end post */}
+        </div>
+      </div>
     </section>
-  )
+  );
 }
+
 
 export default Posts
